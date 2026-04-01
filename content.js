ce5c7ee14025fef4f46c16dc06df7c8fa4793fae
Matthew Parker <matt_parker@outlook.com>
2026-04-01 10:58:01 -0400

    Fix CWS review link, make review prompt permanently dismissable
    - Review URL now uses the actual CWS extension ID slug
    - "Maybe Later" now permanently dismisses (was resetting 7-day timer)
    - Updated content.js version comment to v2.0.3

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// ScriptVault v2.0.0 - Content Script Bridge
+// ScriptVault v2.0.3 - Content Script Bridge
 // Bridges messages between userscripts (USER_SCRIPT world) and background service worker
 
 (function() {
